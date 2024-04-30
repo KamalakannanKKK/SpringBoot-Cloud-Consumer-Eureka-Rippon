00807ff4d6282fe207b3b4fac14a369cae44fae6
KamalakannanKKK <109393676+KamalakannanKKK@users.noreply.github.com>
2024-04-30 16:28:55 +0530

    Add files via upload

---
+++ src/main/java/com/learning/Spring/CloudConsumereurekarippon/SpringCloudConsumerEurekaRipponApplication.java
@@ -0,0 +1,13 @@
+package com.learning.Spring.CloudConsumereurekarippon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConsumerEurekaRipponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConsumerEurekaRipponApplication.class, args);
+	}
+
+}
