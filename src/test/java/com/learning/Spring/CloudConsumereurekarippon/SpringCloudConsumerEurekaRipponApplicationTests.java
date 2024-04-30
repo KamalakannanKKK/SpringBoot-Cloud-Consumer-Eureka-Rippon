ca56e04c0e262baabcd17e79852a248787292b3b
KamalakannanKKK <109393676+KamalakannanKKK@users.noreply.github.com>
2024-04-30 16:30:35 +0530

    Create SpringCloudConsumerEurekaRipponApplicationTests.java

---
+++ src/test/java/com/learning/Spring/CloudConsumereurekarippon/SpringCloudConsumerEurekaRipponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.Spring.CloudConsumereurekarippon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConsumerEurekaRipponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
